1dc6f37ea3b903c8456901c624c70bfdccb92127
SouVitorGabriel <vitor.s.alencar@hotmail.com>
2021-03-24 15:25:09 -0300

    Inicio do projeto

---
+++ DIO.Bank/DIO.Bank/Conta/Conta.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Bank
+{
+    class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+
+        private double Saldo { get; set; }
+
+        private double Credito { get; set; }
+        public string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome )
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            //valida saldo suficiente
+            if(this.Saldo - valorSaque < (this.Credito * -1))
+            {
+                Console.WriteLine("Sem saldo pra isso");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+            Console.WriteLine(this.Nome + "Sacadou: " + valorSaque + ", novo saldo: " + this.Saldo);
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+            Console.WriteLine(this.Nome + " Depositou: " + valorDeposito + ", novo saldo: " + this.Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if(this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Crédito " + this.Credito + " | ";
+
+            return retorno;
+        }
+    }
+}
